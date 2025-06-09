28eaf1d8b243552e545ea5809dcb80c797f4b5a2
kunal pardeshi <pardeshi.kunal.kishor@gmail.com>
2025-06-09 07:59:43 +0530

    edit player with state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let button = <button onClick={handleEditClick}>Edit</button>;
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    button = <button onClick={handleEditClick}>Save</button>;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      {button}
     </li>
   );
 }
